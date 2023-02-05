4ff148e992bc056e0e450635d14be6a27cf9bf07
tanzicai <1728561744@qq.com>
2023-02-05 21:28:34 +0800

    关机自动备份-20230205

--- auto.sh
+++ auto.sh
@@ -1,4 +1,9 @@
-cd ~/Documents/Blog
+cd ~/Documents/DATA/BlogsBack/
 hexo clean
 hexo g
-hexo ss
+hexo d
+echo "博客文件转译上传完成"
+cd ~/Documents/DATA/BlogsBack/
+git add .
+git commit -m "关机自动备份-20$(date +%y%m%d)"
+git push origin main
